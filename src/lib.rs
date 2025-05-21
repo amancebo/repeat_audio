2f62e057dadede371cc3b77f82b6e479c46d4aa1
Angel Mancebo <angel@lepidoptera.local>
2025-05-20 19:37:36 -0500

    Print on success.

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub fn repeat_audio(min_duration_seconds: f64, input_file_name: &str) {
     let n_repeats = (min_duration_seconds / duration_seconds).ceil() as usize;
 
     if n_repeats > 1 {
-        println!("To achieve a duration of at least {min_duration_seconds} seconds, \"{input_file_name}\" will be repeated {n_repeats} times for a final duration of {:0.4} seconds.", (n_repeats * samples.len()) as f64 / sample_rate as f64);
+        println!("To achieve a duration of at least {min_duration_seconds} seconds, '{input_file_name}' will be repeated {n_repeats} times for a final duration of {:0.4} seconds.", (n_repeats * samples.len()) as f64 / sample_rate as f64);
 
         let _duration = n_repeats as f64 * duration_seconds;
         let out_samples: Vec<_> = samples
@@ -29,7 +29,8 @@ pub fn repeat_audio(min_duration_seconds: f64, input_file_name: &str) {
             n_repeats,
             file_extension
         );
-        write(output_file_name, &out_samples, sample_rate, 1).unwrap();
+        write(output_file_name.clone(), &out_samples, sample_rate, 1).unwrap();
+        println!("Saved to '{output_file_name}'");
     } else {
         println!("{min_duration_seconds} seconds is too short; \"{input_file_name}\" would occur only once.\nNo output file will be created.")
     }
